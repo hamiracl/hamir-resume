249f7cf9c280824f28bff61a44a1a0d595a4f46d
hamiracl <70916709+hamiracl@users.noreply.github.com>
2020-12-25 17:24:38 -0500

    changed to phone to string field

--- application/forms.py
+++ application/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, NumberRange
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 
 class ContactForm(FlaskForm):
     email   = StringField("Email", validators=[DataRequired(), Email()])
     name = StringField("Name", validators=[DataRequired(),Length(min=2,max=55)])
-    phone = IntegerField("Phone Number", validators = [NumberRange(min=0, max=15)])
+    phone = StringField("Phone Number", validators = [Length(min=0, max=20)])
     message = TextAreaField("Message", validators=[Length(max = 1000)])
     submit = SubmitField("Send")
